3e5994a86009357eee2977de8ffb6e13a960d798
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-08 11:42:44 +0100

    initial check-in

---
+++ test/acceptance_test.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# shellcheck source=../server.inc
+. "$(dirname "${BASH_SOURCE[0]}")/../server.inc"
+
+GIT_HASH=${GO_REVISION_FNS_GOCD_SERVER_DOCKER:0:7}
+GIT_HASH=${GIT_HASH:-$(git rev-parse --short HEAD)}
+
+IMAGETAG=test-go-server:${GO_PIPELINE_COUNTER:-0}-$GIT_HASH
+
+SHUNIT=$(which shunit)
+
+
+
+function testBuildServer() {
+  buildServer "$IMAGETAG"
+  assertEquals "build server failed" 0 $?
+}
+
+
+
+function testShipServer() {
+  shipServer "$IMAGETAG"
+  assertEquals "ship server failed" 0 $?
+}
+
+
+
+# function oneTimeSetup()
+# function oneTimeTearDown()
+# function setup()
+# function teardown()
+
+
+
+# shellcheck source=/usr/local/bin/shunit
+. "$SHUNIT"
