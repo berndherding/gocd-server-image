deb3b312d28b0136397362c9eaf602f7d88ebb17
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-24 23:49:15 +0100

    check localhost instead of log pattern

--- docker/wait_for_go_server.sh
+++ docker/wait_for_go_server.sh
@@ -4,15 +4,8 @@ i=0
 timeout=$1
 
 while true ; do
-  echo -e -n "\r$i"
-  if [ $i -eq $timeout ] ; then
-    exit 1
-  fi
-  grep 'cachedGoConfigRefreshExecutorThread.*CachedGoConfig.*Finished notifying all listeners' /var/log/go-server/go-server.log &> /dev/null
-  status=$?    
-  if [ $status -eq 0 -a $i -ge 10 ] ; then   # wait a min 10s
-    exit 0
-  fi
-  sleep 1
-  i=$((i + 1))
+  [ $i -ge $timeout ] && exit 1
+  curl -s -o /dev/null http://localhost && exit 0
+  i=$((i + 5))
+  sleep 5
 done
